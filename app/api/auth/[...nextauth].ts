2f011fd9a1c971df6037e5601091ed7f0d46e50b
Nziza-Prince <nzizaprince7@gmail.com>
2025-03-05 20:46:00 +0200

    implemented the auth successfully

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,14 +1,36 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
   try {
-    const { firstname,lastname ,email, password } = await request.json();
+    const { firstname, lastname, email, password } = await request.json();
+
+
+    if (!firstname || !lastname || !email || !password) {
+      return NextResponse.json(
+        { message: "All fields are required." },
+        { status: 400 }
+      );
+    }
+
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User with this email already exists." },
+        { status: 409 } // 409 Conflict
+      );
+    }
+
 
     const hashedPassword = await bcrypt.hash(password, 10);
+
 
     const user = await prisma.user.create({
       data: {
@@ -21,9 +43,9 @@ export async function POST(request: Request) {
 
     return NextResponse.json(user, { status: 201 });
   } catch (error) {
-    console.error(error);
+    console.error("Error creating user:", error);
     return NextResponse.json(
-      { message: "Internal server error" },
+      { message: "Internal server error." },
       { status: 500 }
     );
   }
